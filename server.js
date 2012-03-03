6ce2ea386b27fb0bd9d8a64518ff83f97c95fabe
Koch Michael <kochmichael89@gmail.com>
2012-03-03 23:22:07 +0100

    Renamed stuff, fixed some stuff

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-
 require("cSettings").init(process.env['app_config'], function(cSettings){
 	var http = require("http");
 	var cRouter = require('cRouter');
 	var session = require('cSession');
+   var cpluginloader = require("cPluginLoader");
+   cpluginloader.init();
+   
 	var router = cRouter.route([
 		{ // Create a new router
 			api: 'favicon.ico',
@@ -10,11 +12,11 @@ require("cSettings").init(process.env['app_config'], function(cSettings){
 		callback: require('cFile').staticFile(__dirname + '/client/favicon.ico', 'image/x-icon')
 		}
 		,{
-			api: 'image',
+			api: 'images',
 			method: 'GET',
 			callback: function(req, res){
-				req.cURL.pathname += '.svg';
-				require('cFile').staticDir(__dirname + "/client/core/img", {
+				//req.cURL.pathname += '.svg';
+				require('cFile').staticDir(__dirname + "/client/jquery.mobile-1.0.1/images", { //TODO: register image dirs in package.confs
 					".png": "image/png",
 					".jpg": "image/jpeg",
 					".jpeg": "image/jpeg",
@@ -26,16 +28,15 @@ require("cSettings").init(process.env['app_config'], function(cSettings){
 		,{		// enable for file webserver
 			api: 'html',
 			method: 'GET',
-			callback: require('cFile').staticDir(__dirname,{ '.html': 'text/html', '.css': 'text/css', '.js': 'text/javascript', '.json': 'mime/json', '.mp3': 'audio/mp3'})
+			callback: require('cFile').staticDir(__dirname + '/client',{ '.html': 'text/html', '.css': 'text/css', '.js': 'text/javascript', '.json': 'mime/json', '.mp3': 'audio/mp3'})
 		}
 	]);
-
-	router.add(require("cindex-page").route);
-	router.add(require("cFile").route);
-	var cpluginloader = require("cPluginLoader");
-	cpluginloader.init();
+   
 	router.add(cpluginloader.route);
+   router.add(require("cIndex").route);
+	router.add(require("cFile").route);
 	router.add(require("cDB").route);
+   router.add(require('cOS').route);
 
 	http.createServer(
 		function(req, res){
